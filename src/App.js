0187029d27e366ffc8b159aa5f003b4ca8058979
Dipkul <deepkul2552003@gmail.com>
2023-12-28 11:48:49 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import Products from './components/Products';
-import Product from './components/Product';
-import Cart from './components/Cart';
+import './App.scss';
+// react router v6
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// pages
+import {Home, CategoryProduct, ProductSingle, Cart, Search} from "./pages/index";
+// components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+import store from "./store/store";
+import {Provider} from "react-redux";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-      <Router>
-      <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/products" element={<Products></Products>}></Route>
-          <Route path="/products/:id" element={<Product></Product>}></Route>
-          <Route path="/cart" element=<Cart></Cart>></Route>
-       
-        </Routes>
-      </Router>
+    <div className="App">
+      <Provider store = {store}>
+        <BrowserRouter>
+          <Header />
+          <Sidebar />
+
+          <Routes>
+            {/* home page route */}
+            <Route path = "/" element = {<Home />} />
+            {/* single product route */}
+            <Route path = "/product/:id" element = {<ProductSingle />} />
+            {/* category wise product listing route */}
+            <Route path = "/category/:category" element = {<CategoryProduct />} />
+            {/* cart */}
+            <Route path = "/cart" element = {<Cart />} />
+            {/* searched products */}
+            <Route path = "/search/:searchTerm" element = {<Search />} />
+          </Routes>
+
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import 'font-awesome/css/font-awesome.min.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-    </Provider>
+  </React.StrictMode>
 );
 
-reportWebVitals();
